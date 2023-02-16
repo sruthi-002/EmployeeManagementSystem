05b7b79e99e73876c134885b52d9908ae5c812ce
sruthi-002 <sruthiramesh270@gmail.com>
2023-02-16 12:29:34 +0530

    Added Department , Designation , Project

---
+++ main/java/com/example/demo/Repo/ProjectRepo.java
@@ -0,0 +1,9 @@
+package com.example.demo.Repo;
+
+import com.example.demo.Model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepo extends JpaRepository<Project,Long> {
+}
